adcd6568d3b63efe161a737ee4c88d073e9b1b77
Karen Kirakosyan <kirkaren88@yahoo.com>
2020-05-19 22:47:32 +0400

    Initial commit

---
+++ Calculator.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Karen Kirakosyan on 5/19/2020.
+//
+
+#include "Calculator.h"
+
+double Calculator::Calculate(double x, char oper, double y)
+{
+    switch(oper)
+    {
+        case '+':
+            return x + y;
+        case '-':
+            return x - y;
+        case '*':
+            return x * y;
+        case '/':
+            return x / y;
+        default:
+            return 0;
+    }
+}
---
+++ Calculator.h
@@ -0,0 +1,15 @@
+//
+// Created by Karen Kirakosyan on 5/19/2020.
+//
+
+#ifndef CALCULATOR_CALCULATOR_H
+#define CALCULATOR_CALCULATOR_H
+
+#pragma once
+class Calculator
+{
+public: double Calculate(double x, char oper, double y);
+};
+
+
+#endif //CALCULATOR_CALCULATOR_H
